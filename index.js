9fb9862e8880f1013c8650bd9ee9abde6230228d
KazuoCode <KazuoEdits@gmail.com>
2022-12-10 23:03:07 +0100

    feat: Allow CORS to accept the frontend requests

--- index.js
+++ index.js
@@ -1,6 +1,8 @@
 const express = require('express');
+const cors = require('cors');
 const app = express();
 const port = 3001;
++app.use(cors());
 
 app.get('/search', (req, res) => {
   console.log(req.params);

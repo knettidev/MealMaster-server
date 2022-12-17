8c5025de5356f13f9608e244b2c3b502002e8642
KazuoCode <KazuoEdits@gmail.com>
2022-12-17 21:22:49 +0100

    feat: Added JSON bodyparser for search query

--- index.js
+++ index.js
@@ -1,16 +1,20 @@
+const bodyParser = require('body-parser');
 const express = require('express');
 const cors = require('cors');
 const app = express();
 const port = 3001;
-+app.use(cors());
 
-app.get('/search', (req, res) => {
-  console.log(req.params);
+app.use(cors());
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json());
+
+app.post('/search', (req, res) => {
+  console.log(req.body);
   res.sendStatus(200);
 });
 
-app.get('/surprise', (req, res) => {
-  console.log(req.params);
+app.post('/surprise', (req, res) => {
+  console.log(req.body);
   res.sendStatus(200);
 });
 
